a825cdd551f84f9deabeacb0b15c47e9c184f0ac
Mohammad Ali RAHIMI KOUHBANANI <ext.ma.rahimi@etu.emse.fr>
2025-12-23 02:43:02 +0100

    feat: add Spring Boot initial with dependencies

---
+++ backend/src/main/java/com/cps2/energy/SmartEnergyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cps2.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEnergyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEnergyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cps2/energy/SmartEnergyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cps2.energy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnergyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
