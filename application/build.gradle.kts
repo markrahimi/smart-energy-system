91082a298bb72388d6fa840ae5440155083f09f1
Mohammad Ali RAHIMI KOUHBANANI <ext.ma.rahimi@etu.emse.fr>
2025-12-31 15:30:14 +0100

    feat: add dependency (init android app with compose and navigation)

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -3,3 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+dependencies{
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+}
