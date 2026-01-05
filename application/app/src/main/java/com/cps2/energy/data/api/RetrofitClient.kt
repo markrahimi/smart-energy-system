e80bf0d29647289291346e4c97413513424c9a12
Mohammad Ali RAHIMI KOUHBANANI <ext.ma.rahimi@etu.emse.fr>
2026-01-05 13:26:13 +0100

    feat: add API layer with Moshi and timeout

--- application/app/src/main/java/com/cps2/energy/data/api/RetrofitClient.kt
+++ application/app/src/main/java/com/cps2/energy/data/api/RetrofitClient.kt
@@ -1,16 +1,30 @@
 package com.cps2.energy.data.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://0.0.0.0:8080/api/"
+    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(ApiService::class.java)
     }
