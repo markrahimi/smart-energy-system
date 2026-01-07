8641a95677b459c3e3597c996202a7ae7b83e8fa
Mahdieh Anjomshoae <95015876+mahdiehanjomshoae@users.noreply.github.com>
2026-01-07 15:57:51 +0000

    feat: add Keycloak OAuth2 authentication

--- backend/src/main/java/com/cps2/energy/config/SecurityConfig.java
+++ backend/src/main/java/com/cps2/energy/config/SecurityConfig.java
@@ -1,34 +1,42 @@
 package com.cps2.energy.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+@Profile("!dev")
+class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(csrf -> csrf.disable())  //TODO: CSRF is deactive for test after test change this
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()  // TODO: Any Request have permission for test after test change this
-            );
-
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/api/users/*/notifications/*/read").hasRole("USER")
+                        .requestMatchers("/api/devices/*/deactivate").hasRole("ADMIN")
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> {})
+                )
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
